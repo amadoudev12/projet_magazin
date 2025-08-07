6317fc1565c3ae6619011c79616b2a95490d9c11
amadoudev12 <delhadjamadou670@gmail.com>
2025-08-07 18:12:07 +0100

    modification

--- src/app/client/product/page.tsx
+++ src/app/client/product/page.tsx
@@ -1,17 +1,15 @@
-// "use client"
+// "use client" // <-- Ne pas mettre "use client" si tu utilises Prisma côté serveur
 import prisma from "../../../../lib/prisma";
-import type {product} from "@prisma/client";
+import type { product } from "@prisma/client";
 import BoutiqueProduit from "../../components/BoutiqueProduit";
 
+export const dynamic = 'force-dynamic';  // <<< Ajout ici
+
 export default async function BoutiquePage() {
-    
-    const produits : product[] = await prisma.product.findMany();
-    // typer produits 
+    const produits: product[] = await prisma.product.findMany();
     return (
         <div>
-            <BoutiqueProduit produits = {produits} />
-            {/* <CardCommande order={order} /> */}
+            <BoutiqueProduit produits={produits} />
         </div>
-        
     )
 }
